0139ee0787f5f07244d2956111f995306d044f67
andrey <tirtg@yandex.ru>
2023-03-12 22:39:24 +0300

    added all animal

--- homework_2/Object_all_animal/Dog.java
+++ homework_2/Object_all_animal/Dog.java
@@ -5,20 +5,32 @@ import java.time.LocalDate;
 import homework_2.*;
 
 public class Dog extends Pet {
+    private boolean dogTraining;
 
     public Dog(double height, double weight, String colorEye, String name, String breed, Boolean vaccien,
             String colorWool,
-            LocalDate dateOfBirth) {
+            LocalDate dateOfBirth, boolean dogTraining) {
         super(height, weight, colorEye, name, breed, vaccien, colorWool, dateOfBirth);
+        this.dogTraining = dogTraining;
     }
 
     public Dog() {
-        super(40, 12, "Черные", "Мухтар", "Овчарака", true, "black", LocalDate.now());
+        super(40, 12, "Черные", "Мухтар", "Овчарка", true, "black", LocalDate.now().minusMonths(32));
+        this.dogTraining = true;
     }
 
     @Override
     public void makeSound(Animal target) {
         System.out.println("это " + this.getClass().getSimpleName() + " и он гавкает");
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s\ndogTraining: %s", super.toString(), dogTraining);
+    }
+
+    public void workout() {
+        System.out.println("Идет дрессировка");
     }
 
 }
--- homework_2/Program.java
+++ homework_2/Program.java
@@ -1,20 +1,47 @@
 package homework_2;
 
+import java.text.Bidi;
+
+import homework_2.Object_all_animal.Bird;
 import homework_2.Object_all_animal.Cat;
+import homework_2.Object_all_animal.Chicken;
 import homework_2.Object_all_animal.Dog;
+import homework_2.Object_all_animal.Strok;
+import homework_2.Object_all_animal.Tiger;
+import homework_2.Object_all_animal.Wolf;
 
 public class Program {
 
     public static void main(String[] args) {
-        Cat cat = new Cat();
-        System.out.println(cat);
-        cat.sound();
-        cat.cuddle();
-        System.out.println("-----------");
-        Dog dog = new Dog();
-        System.out.println(dog);
-        dog.cuddle();
-        dog.sound();
+        // Cat cat = new Cat();
+        // System.out.println(cat);
+        // cat.sound();
+        // cat.cuddle();
+        // System.out.println("-----------");
+        // Dog dog = new Dog();
+        // System.out.println(dog);
+        // dog.sound();
+        // dog.cuddle();
+        // dog.workout();
+        // System.out.println("-----------");
+        // Tiger tiger = new Tiger();
+        // System.out.println(tiger);
+        // tiger.sound();
+
+        // Wolf wolf = new Wolf();
+        // System.out.println(wolf);
+        // wolf.sound();
+        // Bird bird = new Bird();
+        // System.out.println(bird);
+        // bird.sound();
+        // bird.displasFlightAltitude();
+        // Chicken chicken = new Chicken();
+        // System.out.println(chicken);
+        // chicken.sound();
+        Strok stork = new Strok();
+        System.out.println(stork);
+        stork.sound();
+        stork.displasFlightAltitude();
 
     }
 }

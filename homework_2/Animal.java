cc8599e8bc87a482e6009bb339c6a305d811f1fb
andrey <tirtg@yandex.ru>
2023-03-22 19:04:39 +0300

    added code for homework_2

--- homework_2/Animal.java
+++ homework_2/Animal.java
@@ -19,7 +19,8 @@ public abstract class Animal implements Make_sound {
 
     @Override
     public String toString() {
-        return String.format("Животное: %s\nheight: %.2f\nweight: %.2f\ncolorEye: %s", this.getClass().getSimpleName(),
+        return String.format("Животное: %s\nheight: %.2f\nweight: %.2f\ncolorEye: %s",
+                this.getClass().getSimpleName(),
                 height, weight, colorEye);
     }
 
--- homework_2/Program.java
+++ homework_2/Program.java
@@ -9,39 +9,15 @@ import homework_2.Object_all_animal.Dog;
 import homework_2.Object_all_animal.Strok;
 import homework_2.Object_all_animal.Tiger;
 import homework_2.Object_all_animal.Wolf;
+import homework_2.Zoo;
+import homework_2.Menu;
 
 public class Program {
-
     public static void main(String[] args) {
-        // Cat cat = new Cat();
-        // System.out.println(cat);
-        // cat.sound();
-        // cat.cuddle();
-        // System.out.println("-----------");
-        // Dog dog = new Dog();
-        // System.out.println(dog);
-        // dog.sound();
-        // dog.cuddle();
-        // dog.workout();
-        // System.out.println("-----------");
-        // Tiger tiger = new Tiger();
-        // System.out.println(tiger);
-        // tiger.sound();
 
-        // Wolf wolf = new Wolf();
-        // System.out.println(wolf);
-        // wolf.sound();
-        // Bird bird = new Bird();
-        // System.out.println(bird);
-        // bird.sound();
-        // bird.displasFlightAltitude();
-        // Chicken chicken = new Chicken();
-        // System.out.println(chicken);
-        // chicken.sound();
-        Strok stork = new Strok();
-        System.out.println(stork);
-        stork.sound();
-        stork.displasFlightAltitude();
+        Zoo zoo = new Zoo();
+        Menu.menu(zoo);
 
     }
+
 }
